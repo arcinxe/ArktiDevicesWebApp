da592119d49b33c4c9fd0bc6658cdcc1d0057942
Marcin Arkan <marcinxe@gmail.com>
2019-05-27 07:59:44 +0200

    Moved from npm to yarn

--- Program.cs
+++ Program.cs
@@ -8,18 +8,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ArktiPhonesWebApp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace ArktiPhonesWebApp {
+    public class Program {
+        public static void Main (string[] args) {
+            CreateWebHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://localhost:5000", "http://192.168.1.7:5000")
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseUrls ("http://localhost:5000", "http://192.168.1.7:5000")
+            .UseStartup<Startup> ();
     }
 }
