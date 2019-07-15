6fc365848002d7726f17d53c16c9380d46f90735
Marcin Arkan <marcinxe@gmail.com>
2019-07-15 07:25:10 +0200

    Update the main page

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import { NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>Hello!</h1>
+        <p>Let's check how the parameters of the mobile devices have been changing over time!</p>
+        <p>Features:</p>
         <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li><strong>Displaying data using colorful bar charts</strong></li>
+          <li><strong>Checking details of each part of the graphs</strong></li>
+          <li><strong>Filtering the brands you like</strong></li>
         </ul>
-        <p>To help you get started, we have also set up:</p>
+        <p>To get started, chose one of the currently available charts below or from the top navigation bar!</p>
         <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+          <li><Link to="/minijack">Mini jack</Link> - check which phones have the 3.5mm audio jack</li>
+          <li><Link to="/infrared">Infrared</Link> - see which modern devices are able to control your TV</li>
+          <li><Link to="/ram">RAM</Link> - display the devices that are good for multitasking</li>
         </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <p>Many more interesting graphs are currently in development!</p>
       </div>
     );
   }
